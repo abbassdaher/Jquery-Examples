e784ab240f2af5a8e5a2b72028118012258ed473
abbassDaher <abbassDaher1@outlook.com>
2023-10-04 12:02:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Header Style App
+$(".header-list li").click(function () { 
+    $(this).addClass('active').siblings().removeClass('active');
+    // console.log($(this).attr("href", "home"))
+    
+});
+// Window App
+$(window).scroll(function () { 
+    console.log("yes");
+});
+// Dynamic Taps
+// Filter Images
+// Smooth Scroll Header App
+// Scroll To Top App
+// Calculate | CountDown Characters of Textarea App
