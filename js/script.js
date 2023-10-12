89df78ace8e24c7bff5e1193469c3a8cfd5b59d7
abbassDaher <abbassDaher1@outlook.com>
2023-10-12 13:07:57 +0300

    scroll to top section

--- js/script.js
+++ js/script.js
@@ -1,49 +1,61 @@
 // Header Style App
-$(".header-list li").click(function () { 
+$(".header-list li").click(function () {
     $(this).addClass('active').siblings().removeClass('active');
     // console.log($(this).attr("href", "home"))
-    
+
 });
 // Window App
 var homeTopOffset = $(".home").offset().top;
 console.log(homeTopOffset);
-$(window).scroll(function () { 
+$(window).scroll(function () {
 
-    if($(window).scrollTop() > 0){
+    if ($(window).scrollTop() > 0) {
         $("header").css({
             "opacity": "0.6",
             "box-shadow": "2px 2px 5px"
         });
     }
-    else{
+    else {
         $("header").css({
             "opacity": "1",
             "box-shadow": ""
-        }); 
+        });
     }
     console.log("yes");
 });
 // Dynamic Taps
 $("body section").show()
-$(".logo a").click(function(){
+$(".logo a").click(function () {
     $("body section").show()
 })
-  $(".header-list li a" ).click(function(){
+$(".header-list li a").click(function () {
     var id = $(this).attr("id");
-    $("body section").hide();
-    $("."+id).fadeIn()
-    console.log("."+id);
-  });
+    // $("body section").hide();
+    $("." + id).fadeIn()
+    console.log("." + id);
+});
 // Filter Images
 $(".bullet li").click(function () {
     var getID = $(this).attr("id")
-    var img = $("images-list").find('div').attr("class","getID");
-    $("."+getID).siblings().hide()
-    $("."+getID).show()
-    $("#"+getID +" i").addClass("fa fa-circle")
-    console.log("."+getID);
+    var img = $("images-list").find('div').attr("class", "getID");
+    $("." + getID).siblings().hide()
+    $("." + getID).fadeIn()
+    $("#" + getID + " i").addClass("fa fa-circle")
+    console.log("." + getID);
 }
 );
 // Smooth Scroll Header App
 // Scroll To Top App
+var headerHeight = $("header").innerHeight();
+$(".header-list li a").click(function () {
+    let id = $(this).attr("id")
+    $("body,html").animate(
+        {
+            scrollTop: $("."+id).offset().top-headerHeight
+                    }
+    )
+
+
+
+});
 // Calculate | CountDown Characters of Textarea App
